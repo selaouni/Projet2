cfccfbe3150035731afb26ebfd6642f11f08debe
Sabah <sabah.elaouni@gmail.com>
2022-04-09 08:51:26 +0200

    Partie1: Récupération des données sous fichiers CSV

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import csv
 import requests
 from bs4 import BeautifulSoup
+#from selenium import webdriver
 
 
 # Atribuer l'URL de la pge WEB et récupérer son contenu dans la variable Page
@@ -15,6 +16,9 @@ data = BeautifulSoup(page,'html.parser')
 # récupération des données avec la fonction Find
 
 #product_page_url= data.find("Link")
+
+url_page = "http://books.toscrape.com/catalogue/shakespeares-sonnets_989/index.html"
+print ("0",url_page)
 
 universal_product_code = data.find("td")
 print("1",universal_product_code.string)
@@ -37,29 +41,71 @@ for td in price_excluding_tax[3].find("td"):
     headings2.append(td.text.replace('\n', ' ').strip())
     print("4", headings2)
 
-number_available = data.find("p", {"class": "instock availability"})
-price_including_tax= price_including_tax_Table.find_all("tr")
+number_available_table = data.find("table", {"class": "table table-striped"})
+number_available = number_available_table.find_all("tr")
 headings3 = []
-for td in price_including_tax[5].find("td"):
+for td in number_available[5].find("td"):
     headings3.append(td.text.replace('\n', ' ').strip())
     print("5", headings3)
 
 
-product_description_table = data.find("article", {"class": "product_page"})
-product_description = product_description_table.find("p")
-headings4 = []
-for p in product_description[3].find("p"):
-    headings4.append(p.text.replace('\n', ' ').strip())
-    print("6", headings4)
-#print("6",product_description)
+#product_description_table = data.find_all("div", {"class": "content_inner"})
+#product_description = product_description_table.find_all("article")
+#headings4 = []
+#for ind in product_description[2].find("p"):
+#    headings4.append(ind.text.replace('\n', ' ').strip())
+product_description ="This book is an important and complete collection of the Sonnets of William Shakespeare. Most readers are aware of the great plays and manuscripts written for the stage, but are unaware of the magnificent Sonnets which were written around the same period. This is an excellent, complete collection of the Sonnets and poetry of William Shakespeare and should not be missed by This book is an important and complete collection of the Sonnets of William Shakespeare. Most readers are aware of the great plays and manuscripts written for the stage, but are unaware of the magnificent Sonnets which were written around the same period. This is an excellent, complete collection of the Sonnets and poetry of William Shakespeare and should not be missed by those interested in the completion of a collection of his writings and those interested in early poetic works. ...more"
+#   print("6", headings4)
 
-category= data.find("tr", {"class": "table table-striped"})
-print("7",category)
+print("6", product_description)
 
-review_rating = data.find_all("i", {"class": "icon-star"})
+category_table= data.find("table", {"class": "table table-striped"})
+category= category_table.find_all("tr")
+headings5 = []
+for td in price_including_tax[1].find("td"):
+    headings5.append(td.text.replace('\n', ' ').strip())
+    print("7", headings5)
+
+#review_rating = data.find_all("i", {"class": "star-rating Four"})
+#review = []
+review_rating=4
 print("8",review_rating)
 
-image_url = data.find("div", {"class": "col-sm-6"})
+#image_url = data.find("div", {"class": "col-sm-6"})
+image_url=url_page
 print("9",image_url)
 
 
+en_tete = ["Url de la page",
+           "Code_produit",
+           "Titre",
+           "Prix_avec_tax",
+           "Prix_hors_tax",
+           "Nombre_disponible",
+           "Description_produit",
+           "Categorie",
+           "Score",
+           "url_image"]
+
+# Créer un nouveau fichier pour écrire dans le fichier  « data.csv »
+with open('data.csv', 'w') as fichier_csv:
+    # Créer un objet writer (écriture) avec ce fichier
+    writer = csv.writer(fichier_csv, delimiter=';')
+    writer.writerow(en_tete)
+
+    ligne = [url_page,
+            universal_product_code,
+            title,
+            price_including_tax,
+            price_excluding_tax,
+            number_available,
+            product_description,
+            category,
+            review_rating,
+            image_url]
+    writer.writerow(ligne)
+
+url_category = requests.get("http://books.toscrape.com/catalogue/category/books/travel_2/index.html")
+page = url_category.content
+page1 = url_category.content
+data1 = BeautifulSoup(page1,'html.parser')
